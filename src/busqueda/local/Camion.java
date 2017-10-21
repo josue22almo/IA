fe51eaabe17260bb448e9ae49a89a351c576b30a
josue22almo <josue.almo@gmail.com>
2017-10-21 18:37:14 +0200

    HOTFIX: calculo correcto de la distancia

--- src/busqueda/local/Camion.java
+++ src/busqueda/local/Camion.java
@@ -2,7 +2,6 @@ package busqueda.local;
 
 import IA.Gasolina.Gasolinera;
 
-import java.util.ArrayList;
 
 public class Camion{
     private int viajes;
@@ -14,6 +13,7 @@ public class Camion{
     private int coordY;
     private double ingresos;
     private double gastos;
+    private int peticionesAtendidas;
     private static int PRECIOKM = 2;
     private static int GANANCIAPORTANQUE = 1000;
 
@@ -24,22 +24,27 @@ public class Camion{
         tanques = 2;
         distanciaDisponible = 640;
         gastos = ingresos = 0;
+        peticionesAtendidas = 0;
     }
 
     public void volverAlCentroDeDistribucion(){
-        --viajes;
-        tanques = 2;
-        gastos += 2*calcularDistancia(coordsCentreX, coordsCentreY);
-        this.coordX = this.coordsCentreX;
-        this.coordX = this.coordsCentreX;
+        if (this.coordX != this.coordsCentreX && this.coordY != this.coordsCentreY) {
+            viajes = --viajes;
+            tanques = 2;
+            gastos += 2 * calcularDistancia(coordsCentreX, coordsCentreY);
+            distanciaDisponible -=  calcularDistancia(coordsCentreX, coordsCentreY);
+            this.coordX = this.coordsCentreX;
+            this.coordY = this.coordsCentreY;
+        }
     }
 
     public double calcularDistancia(int destX, int destY){
-        return Math.sqrt((coordX-destX)^2+(coordY-destY)^2);
+        return Math.sqrt(Math.pow(coordX-destX, 2) + Math.pow(coordY-destY, 2));
     }
 
     public void atenderPeticion(Gasolinera gasolinera, int numPet){
         double percent;
+        ++peticionesAtendidas;
         int dias = gasolinera.getPeticiones().get(numPet);
         if(dias == 0) percent = 102;
         else percent = 100 - Math.pow(2, dias);
@@ -48,6 +53,8 @@ public class Camion{
         gastos += PRECIOKM*distRecorrida;
         distanciaDisponible -= distRecorrida;
         tanques--;
+        coordY = gasolinera.getCoordY();
+        coordX = gasolinera.getCoordX();
     }
 
     public double getBeneficiosNetos(){
@@ -55,7 +62,7 @@ public class Camion{
     }
 
     public boolean puedoAtenderPeticion(Gasolinera gasolinera){
-        return tanques > 0 && 2*calcularDistancia(gasolinera.getCoordX(), gasolinera.getCoordY()) <= distanciaDisponible;
+        return viajes > 0 && tanques > 0 && 2*calcularDistancia(gasolinera.getCoordX(), gasolinera.getCoordY()) <= distanciaDisponible;
     }
 
     public double getIngresos() {
@@ -64,5 +71,17 @@ public class Camion{
 
     public double getGastos() {
         return gastos;
+    }
+
+    public int getViajes() {
+        return viajes;
+    }
+
+    @Override
+    public String toString() {
+        return "origen = (" + String.valueOf(coordsCentreX)+ "," + String.valueOf(coordsCentreY) + ") - actual (" + String.valueOf(coordX)+ "," + String.valueOf(coordY) + ")"
+                + " viajes disponibles " + viajes + " tanques disponibles " + String.valueOf(tanques) +  " peticones atendidas " +
+                String.valueOf(peticionesAtendidas) + " ganancias = " + String.valueOf(getBeneficiosNetos()) + " km restantes " +
+                String.valueOf(distanciaDisponible);
     }
 }
